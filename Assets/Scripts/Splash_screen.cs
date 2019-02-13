ef36599366593e4d91f5332b9f8ac2fe28dfb144
Pekka Koukkula <pekka.koukkula@genisys.fi>
2019-02-13 10:19:52 +0200

    Added terrain, splash and player model

--- Assets/Scripts/Splash_screen.cs
+++ Assets/Scripts/Splash_screen.cs
@@ -1,6 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Splash_screen : MonoBehaviour
 {
@@ -13,6 +12,9 @@ public class Splash_screen : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Time.time >= 4)
+        {
+            SceneManager.LoadScene(1);
+        }
     }
 }

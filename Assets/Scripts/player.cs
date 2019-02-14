d3698ffc0e8b3c7becffba42161dedcef223a819
Pekka Koukkula <pekka.koukkula@genisys.fi>
2019-02-14 14:16:58 +0200

    added ship controls and motion

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -1,10 +1,23 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class player : MonoBehaviour
 {
 
-    [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 4f;
+    [Tooltip("In ms^-1")] [SerializeField] float Speed = 4f;
+    [SerializeField] float xRange = 4f;
+    [SerializeField] float yRange = 2.4f;
+
+    [SerializeField] float positionPitchFactor = -10f;
+    [SerializeField] float controlPitchFactor = -20f;
+
+    [SerializeField] float positionYawFactor = 8f;
+
+    [SerializeField] float controlRollFactor = -20f;
+
+    float xThrow, yThrow;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,8 +27,44 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float xOffsetThisFrame = xThrow * xSpeed * Time.deltaTime;
-        print(xOffsetThisFrame);
+        moveShipInX();
+        moveShipInY();
+        processRotation();
+    }
+
+    private void processRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+
+        float yawDueToPosition = transform.localPosition.x * positionYawFactor;
+        float yaw = yawDueToPosition;
+
+        float roll = xThrow * controlRollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void moveShipInX()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // This is between 0 and 1, static and fully sideway
+        float xOffset = xThrow * Speed * Time.deltaTime; // 0-1 * 4 * time since current and last frame
+        float rawNewXPos = transform.localPosition.x + xOffset;
+        float xPos = Mathf.Clamp(rawNewXPos, -xRange, xRange);
+
+
+        transform.localPosition = new Vector3(xPos, transform.localPosition.y, transform.localPosition.z);
+    }
+
+    private void moveShipInY()
+    {
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical"); // This is between 0 and 1, static and fully up/down
+        float yOffset = yThrow * Speed * Time.deltaTime; // 0-1 * 4 * time since current and last frame
+        float rawNewYPos = transform.localPosition.y + yOffset;
+        float yPos = Mathf.Clamp(rawNewYPos, -yRange, yRange);
+
+
+        transform.localPosition = new Vector3(transform.localPosition.x, yPos, transform.localPosition.z);
     }
 }

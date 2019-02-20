8747240b3eface269066f1abbf1fb8c08ff56047
Pekka Koukkula <pekka.koukkula@genisys.fi>
2019-02-20 13:27:18 +0200

    Added enemy death and explosions

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    Collider enemyBoxCollider;
+    [SerializeField] GameObject deathFX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        addBoxCollider();
+    }
+
+    void addBoxCollider()
+    {
+        enemyBoxCollider = gameObject.AddComponent<BoxCollider>();
+        enemyBoxCollider.isTrigger = false;
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        print(gameObject.name +" was hit by a bullet!");
+        Instantiate(deathFX, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
